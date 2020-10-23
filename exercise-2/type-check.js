571e9bcdf1d72421c33fd160c2b5875e46cf423a
Martin <martinpetit1998@gmail.com>
2020-10-23 14:59:22 +0200

    Ajout exo 1 et 2 typecheck

---
+++ exercise-2/type-check.js
@@ -0,0 +1,47 @@
+
+
+function type_check_v1(value, type) {
+
+    if (type === 'null' ) {
+        return value === null;
+    } else if (type === 'object' && typeof value === 'object') {
+        return true;
+    } else if (type === 'number' && typeof value === 'number') {
+        return true;
+    } else if (type === 'string' && typeof value === 'string') {
+        return true;
+    } else if (type === 'array' && Array.isArray(value)) {
+        return true;
+    } else if (type === 'symbol' && typeof value === 'symbol') {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+
+function type_check_v2(value, conf) {
+    for (let key in conf) {
+        if (key === 'type') {
+            if (!type_check_v1(value, conf[key])) {
+                return false;
+            }
+        } else if (key === 'value') {
+            if (conf[key] !== value) {
+                return false;
+            }
+        } else if (key === 'enum') {
+            if (!conf[key].includes(value)) {
+                return false;
+            }
+        }
+
+    }
+
+    return true;
+}
+
+function type_check() {
+    
+}
